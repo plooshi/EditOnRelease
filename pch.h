789e0ef7112242305535615e4905ec61b69aabf8
Ploosh <me@ploosh.dev>
2024-06-17 12:15:15 -0400

    Remove unused headers

--- pch.h
+++ pch.h
@@ -12,11 +12,8 @@
 #include "plooshfinder/include/formats/pe.h"
 #include "plooshfinder/include/plooshfinder8.h"
 #include "plooshfinder/include/plooshfinder.h"
-#include <string_view>
 #include <array>
-#include "plooshfinder/include/plooshfinder_sig.h"
 #include <string>
-#include <algorithm>
 
 enum EActivationMethod {
 	CommandLine,
